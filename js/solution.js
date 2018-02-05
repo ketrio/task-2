00f90326f34b7d078185de78068e5acf41216d8a
Vlad <vladgutkovskii@gmail.com>
2018-02-05 14:45:12 +0300

    implement solution

--- js/solution.js
+++ js/solution.js
@@ -10,8 +10,31 @@
      * @returns {number} кол-во островов
      */
     function solution(map) {
-        // todo: подсчитать кол-во островов на карте
-        return 0;
+        const visited = map.map(row => row.map(() => false));
+
+        function dfs(i, j) {
+            const allowed = (i, j) => map[i][j] === ISLAND && !visited[i][j];
+            const between = (left, right, x) => left <= x && x < right;            
+    
+            if (
+                !between(0, map.length, i) 
+                || !between(0, map[0].length, j)
+                || !allowed(i, j)
+            ) {
+                return 0; 
+            }
+            visited[i][j] = true;
+            [
+                {i: i - 1, j},
+                {i: i + 1, j},
+                {i, j: j + 1},
+                {i, j: j - 1},
+            ].forEach(isl => dfs(isl.i, isl.j));
+            return 1;
+        }
+
+        return map.map((row, i) => row.reduce((acc, cur, j) => acc + dfs(i, j), 0))
+            .reduce((acc, e) => acc += e);
     }
 
     root.SHRI_ISLANDS.solution = solution;
